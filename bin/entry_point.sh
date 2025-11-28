c26d4387754f28723ee92f26f6c12925a0f0dd12
f-allian <allianfarhad@gmail.com>
2025-11-28 14:30:12 +0000

    fix: docker scripts

---
+++ bin/entry_point.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Entry point script running"
+
+CONFIG_FILE=_config.yml
+
+# No Gemfile.lock management
+manage_gemfile_lock() {
+    echo "Skipping Gemfile.lock"
+}
+
+start_jekyll() {
+    manage_gemfile_lock
+    bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling &
+}
+
+start_jekyll
+
+while true; do
+    inotifywait -q -e modify,move,create,delete $CONFIG_FILE
+    if [ $? -eq 0 ]; then
+        echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+        jekyll_pid=$(pgrep -f jekyll)
+        kill -KILL $jekyll_pid
+        start_jekyll
+    fi
+done
